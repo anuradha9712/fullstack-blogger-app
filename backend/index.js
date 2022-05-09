9430a64e6abfe2802903a80cff6cfe629a0e3ef6
Anuradha Aggarwal <I1333@IT002600.local>
2022-05-09 23:31:13 +0530

    chore: heroku setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const logger = require('./utils/logger');
 const blogRouter = require('./controllers/blog');
 const middleware = require('./utils/middleware');
