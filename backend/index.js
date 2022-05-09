ee8520ee7d2a5bb5567e3fdbd256ce311b2f8e18
Anuradha Aggarwal <I1333@IT002600.local>
2022-05-10 00:11:37 +0530

    chore: deploy production build

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware');
 const cors = require('cors')
 
 app.use(cors());
+app.use(express.static('build'));
 
 // it takes the JSON data of a request, transforms it into a JavaScript object 
 // and then attaches it to the body property of the request object before the route handler is called.
